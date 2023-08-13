acef508f9c5ca4125eda1ca00fb2086c06d59fa8
hxrxchang <mail@hxrxchang.dev>
2023-08-13 23:01:49 +0900

    get issues

--- cmd/gtd/main.go
+++ cmd/gtd/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/google/go-github/v53/github"
+	"golang.org/x/oauth2"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	token := os.Getenv("GITHUB_ACCESS_TOKEN")
+	username := os.Getenv("GITHUB_USERNAME")
+	repo := os.Getenv("GITHUB_REPO")
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
+
+	issues, _, err :=client.Issues.ListByRepo(ctx, username, repo, nil)
+	if err != nil {
+		fmt.Printf("Issues.Get returned error: %v\n", err)
+		os.Exit(1)
+	}
+	issue := issues[0]
+	fmt.Printf("Body: \n%v\n", *issue.Body)
 }
