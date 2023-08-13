1f5a265e5e2444f61353793f29656d250a8ce0db
hxrxchang <mail@hxrxchang.dev>
2023-08-14 00:59:08 +0900

    parse markdown

--- cmd/gtd/main.go
+++ cmd/gtd/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"os"
 
 	"github.com/google/go-github/v53/github"
+	"github.com/yuin/goldmark"
+	"github.com/yuin/goldmark/extension"
 	"golang.org/x/oauth2"
 )
 
@@ -13,6 +16,22 @@ func main() {
 	token := os.Getenv("GITHUB_ACCESS_TOKEN")
 	username := os.Getenv("GITHUB_USERNAME")
 	repo := os.Getenv("GITHUB_REPO")
+
+	if token == "" {
+		fmt.Println("GITHUB_ACCESS_TOKEN is required")
+		os.Exit(1)
+	}
+
+	if username == "" {
+		fmt.Println("GITHUB_USERNAME is required")
+		os.Exit(1)
+	}
+
+	if repo == "" {
+		fmt.Println("GITHUB_REPO is required")
+		os.Exit(1)
+	}
+
 	ctx := context.Background()
 	ts := oauth2.StaticTokenSource(
 		&oauth2.Token{AccessToken: token},
@@ -26,5 +45,12 @@ func main() {
 		os.Exit(1)
 	}
 	issue := issues[0]
-	fmt.Printf("Body: \n%v\n", *issue.Body)
+
+	md := goldmark.New(goldmark.WithExtensions(extension.GFM))
+	var buf bytes.Buffer
+	if err := md.Convert([]byte(*issue.Body), &buf); err != nil {
+		fmt.Printf("md.Convert returned error: %v\n", err)
+		os.Exit(1)
+	  }
+	fmt.Println(buf.String())
 }
