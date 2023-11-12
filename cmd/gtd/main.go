09e3ac603045b6ac1fbc81a924fd1467a4bd1ef6
hxrxchang <mail@hxrxchang.dev>
2023-11-12 22:24:02 +0900

    get issue data

--- cmd/gtd/main.go
+++ cmd/gtd/main.go
@@ -38,17 +38,35 @@ func main() {
 	httpClient := oauth2.NewClient(ctx, src)
 	client := githubv4.NewClient(httpClient)
 
+	variables := map[string]interface{}{
+		"name":  githubv4.String(repo),
+		"owner": githubv4.String(username),
+	}
+
 	var query struct {
 		Viewer struct {
 			Login     githubv4.String
 			CreatedAt githubv4.DateTime
 		}
+		Repository struct {
+			Issues struct {
+				Edges []struct {
+					Node struct {
+						Title githubv4.String
+						Body  githubv4.String
+					}
+				}
+			} `graphql:"issues(first: 1, states: OPEN, orderBy: {field: CREATED_AT, direction: DESC})"`
+		} `graphql:"repository(owner: $owner, name: $name)"`
 	}
 
-	err := client.Query(context.Background(), &query, nil)
+	err := client.Query(context.Background(), &query, variables)
 	if err != nil {
+		fmt.Println(err)
 		// Handle error.
 	}
 	fmt.Println("    Login:", query.Viewer.Login)
 	fmt.Println("CreatedAt:", query.Viewer.CreatedAt)
+	fmt.Println("Title", query.Repository.Issues.Edges[0].Node.Title)
+	fmt.Println("Body", query.Repository.Issues.Edges[0].Node.Body)
 }
