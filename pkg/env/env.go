ee1f62f5a4505d00a116c13953baa6a5ac5f43f3
hxrxchang <mail@hxrxchang.dev>
2023-11-19 12:57:51 +0900

    fix error handling

--- pkg/env/env.go
+++ pkg/env/env.go
@@ -13,10 +13,16 @@ func GetGitHubInfo() (string, string, string, error) {
 
 	if token == "" {
 		err = fmt.Errorf("ACCESS_TOKEN is required")
+		return "", "", "", err
+	}
+
+	if ghrepo == "" {
+		err = fmt.Errorf("REPOSITORY is required")
+		return "", "", "", err
 	}
 
 	username := strings.Split(ghrepo, "/")[0]
 	repo := strings.Split(ghrepo, "/")[1]
 
-	return token, username, repo, err
+	return token, username, repo, nil
 }
