fca6caeb704b5dfa8d65bc158f7fea6a021456f0
hxrxchang <mail@hxrxchang.dev>
2023-12-09 00:51:12 +0900

    fix error handling

--- pkg/env/env.go
+++ pkg/env/env.go
@@ -18,8 +18,13 @@ func GetGitHubInfo() (string, string, string, error) {
 		return "", "", "", fmt.Errorf("GITHUB_REPOSITORY is required")
 	}
 
-	username := strings.Split(ghrepo, "/")[0]
-	repo := strings.Split(ghrepo, "/")[1]
+	splited := strings.Split(ghrepo, "/")
+	if len(splited) != 2 {
+		return "", "", "", fmt.Errorf("GITHUB_REPOSITORY is invalid")
+	}
+
+	username := splited[0]
+	repo := splited[1]
 
 	return token, username, repo, nil
 }
